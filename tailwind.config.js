8eec0a051959c503a10f080a4519003c24045fc8
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2023-12-03 18:02:17 +0100

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets//*.js", "./templates//*.html.twig"],
+  content: ["**/**.html.twig"],
   theme: {
     extend: {},
   },
