f1f531571dfbe2a058e3f18374cef3e10271bd33
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2023-12-08 00:13:07 +0100

    Front Edit and Show modal implemented

--- public/js/customer.js
+++ public/js/customer.js
@@ -47,28 +47,41 @@ function openCustomerModal(customerId) {
     .then((response) => response.json())
     .then((data) => {
       const modalBody = document.querySelector(
-        "#customerDetailsModal .modal-body"
+        "#customerDetailsModalContainer"
       );
       let customerDetails;
 
+      customerDetails = `<button class="absolute top-3 right-3 text-white font-medium bg-red-500 hover:bg-red-700 transition-all duration-300 ease-out rounded-lg px-2 py-1" onclick="toggleModal('customerDetailsModal', 'customerDetailsModalContainer')">
+        Fermer
+      </button>`;
+
       if (data.nameSociety) {
-        customerDetails = `<p>Société: ${data.nameSociety}</p>`;
+        customerDetails += `<div class="flex flex-col justify-start items-start mt-3"> <p class="font-semibold">Société:
+        </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.nameSociety}</p> </div>`;
       } else {
-        customerDetails = `<p>Nom: ${data.name}</p> <p>Prénom: ${data.lastName}</p>`;
+        customerDetails += `<div class="flex flex-col justify-start items-start mt-3"> <p class="font-semibold">Nom:
+        </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.name}</p> <p>Prénom: ${data.lastName}</p> </div>`;
       }
 
-      customerDetails += `<p>Ville: ${data.city}</p>`;
-      customerDetails += `<p>Adresse: ${data.streetNumber} ${data.streetName}, ${data.postalCode}</p>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Ville:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.city}</p> </div>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Adresse:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.streetNumber} ${data.streetName}, ${data.postalCode}</p> </div>`;
 
-      customerDetails += `<p>Devis en attente: ${data.devisCounts.pending}</p>`;
-      customerDetails += `<p>Devis payés: ${data.devisCounts.paid}</p>`;
-      customerDetails += `<p>Devis partiellement payés: ${data.devisCounts.partial}</p>`;
-      customerDetails += `<p>Devis remboursés: ${data.devisCounts.refunded}</p>`;
-      customerDetails += `<p>Total de devis: ${data.totalDevisCount}</p>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Devis en attente:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.devisCounts.pending}</p> </div>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Devis payés:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.devisCounts.paid}</p> </div>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Devis partiellement payés:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.devisCounts.partial}</p> </div>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Devis remboursés:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.devisCounts.refunded}</p> </div>`;
+      customerDetails += `<div class="flex flex-col justify-start items-start"> <p class="font-semibold">Total de devis:
+      </p> <p class="'rounded-xl w-full h-10  p-1 border border-solid border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"> ${data.totalDevisCount}</p> </div>`;
 
       modalBody.innerHTML = customerDetails;
 
-      $("#customerDetailsModal").modal("show");
+      toggleModal("customerDetailsModal", "customerDetailsModalContainer");
     })
     .catch((error) =>
       console.error("Erreur lors de la récupération des données:", error)
@@ -79,12 +92,10 @@ function openCustomerEditModal(customerId) {
   fetch(`/customer/${customerId}/edit`)
     .then((response) => response.text())
     .then((html) => {
-      const modalBody = document.querySelector(
-        "#customerEditModal .modal-body"
-      );
+      const modalBody = document.querySelector("#customerEditModalContent");
       modalBody.innerHTML = html;
       initializeFormFields();
-      $("#customerEditModal").modal("show");
+      toggleModal("customerEditModal", "customerEditModalContent");
     })
     .catch((error) =>
       console.error("Erreur lors de la récupération du formulaire:", error)
@@ -100,7 +111,6 @@ function openCustomerCreateModal() {
       modalBody.innerHTML = html;
       initializeFormFields();
 
-      // Cette ligne doit être mise à jour pour correspondre à la manière dont vous affichez votre modal dans Twig
       toggleModal("newModalId", "newModalModalContentId");
     })
     .catch((error) =>
