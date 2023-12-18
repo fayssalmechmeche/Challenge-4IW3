14f380e6ecd0af05c21a1237ee0772a52a54f5ef
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2023-12-18 12:05:17 +0100

    fix produit

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -107,6 +107,7 @@ class CustomerController extends AbstractController
         return $this->render('customer/new.html.twig', [
             'customer' => $customer,
             'form' => $form->createView(),
+            'form_action' => $this->generateUrl('app_customer_new')
         ]);
     }
 
