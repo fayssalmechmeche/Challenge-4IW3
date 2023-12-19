5ffd249947dc3d749e2cc235bd58242489f8e8f6
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2023-12-19 10:29:14 +0100

    add Faker and loop

--- src/DataFixtures/CreateUsersFixtures.php
+++ src/DataFixtures/CreateUsersFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use DateTime;
+use Faker\Factory;
 use App\Entity\User;
 use App\Entity\Society;
 use Doctrine\Persistence\ObjectManager;
@@ -17,62 +18,66 @@ class CreateUsersFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
-        $society = new Society();
-        $society->setName('Nom de société');
-        $society->setAddress('Adresse');
-        $society->setPhone('0123456789');
-        $society->setEmail('email@example.com');
-        $society->setSiret('siret');
+        $faker = Factory::create('fr_FR');
 
-        $manager->persist($society);
+        for ($i = 0; $i < 10; $i++) {
+            $society = new Society();
+            $society->setName($faker->company());
+            $society->setAddress($faker->address());
+            $society->setPhone($faker->phoneNumber());
+            $society->setEmail('email' . $i . '@example.com');
+            $society->setSiret('siret');
 
-        $admin = new User();
-        $admin->setName('John');
-        $admin->setLastName('Admin');
-        $admin->setEmail('admin@gmail.com');
-        $admin->setPassword($this->passwordHasher->hashPassword($admin, SELF::PASSWORD));
-        $admin->setCreatedAt(new DateTime());
-        $admin->setSociety($society);
-        $admin->setIsVerified(true);
-        $admin->setRoles(["ROLE_ADMIN"]);
-
-        $accountant = new User();
-        $accountant->setName('John');
-        $accountant->setLastName('Comptable');
-        $accountant->setEmail('comptable@gmail.com');
-        $accountant->setPassword($this->passwordHasher->hashPassword($accountant, SELF::PASSWORD));
-        $accountant->setCreatedAt(new DateTime());
-        $accountant->setSociety($society);
-        $accountant->setIsVerified(true);
-        $accountant->setRoles(["ROLE_ACCOUNTANT"]);
-
-        $societyUser = new User();
-        $societyUser->setName('John');
-        $societyUser->setLastName('Entreprise');
-        $societyUser->setEmail('entreprise@gmail.com');
-        $societyUser->setPassword($this->passwordHasher->hashPassword($societyUser, SELF::PASSWORD));
-        $societyUser->setCreatedAt(new DateTime());
-        $societyUser->setSociety($society);
-        $societyUser->setIsVerified(true);
-        $societyUser->setRoles(["ROLE_SOCIETY"]);
-
-
-        $user = new User();
-        $user->setName('John');
-        $user->setLastName('Utilisateur');
-        $user->setEmail('user@gmail.com');
-        $user->setPassword($this->passwordHasher->hashPassword($user, SELF::PASSWORD));
-        $user->setCreatedAt(new DateTime());
-        $user->setSociety($society);
-        $user->setIsVerified(true);
+            $manager->persist($society);
 
 
 
-        $manager->persist($user);
-        $manager->persist($admin);
-        $manager->persist($accountant);
-        $manager->persist($societyUser);
+            $admin = new User();
+            $admin->setName($faker->firstName());
+            $admin->setLastName($faker->lastName());
+            $admin->setEmail('admin' . $i . '@gmail.com');
+            $admin->setPassword($this->passwordHasher->hashPassword($admin, SELF::PASSWORD));
+            $admin->setCreatedAt(new DateTime());
+            $admin->setSociety($society);
+            $admin->setIsVerified(true);
+            $admin->setRoles(["ROLE_ADMIN"]);
 
-        $manager->flush();
+
+            $accountant = new User();
+            $accountant->setName($faker->firstName());
+            $accountant->setLastName($faker->lastName());
+            $accountant->setEmail('comptable' . $i . '@gmail.com');
+            $accountant->setPassword($this->passwordHasher->hashPassword($accountant, SELF::PASSWORD));
+            $accountant->setCreatedAt(new DateTime());
+            $accountant->setSociety($society);
+            $accountant->setIsVerified(true);
+            $accountant->setRoles(["ROLE_ACCOUNTANT"]);
+
+
+            $societyUser = new User();
+            $societyUser->setName($faker->firstName());
+            $societyUser->setLastName($faker->lastName());
+            $societyUser->setEmail('entreprise' . $i . '@gmail.com');
+            $societyUser->setPassword($this->passwordHasher->hashPassword($societyUser, SELF::PASSWORD));
+            $societyUser->setCreatedAt(new DateTime());
+            $societyUser->setSociety($society);
+            $societyUser->setIsVerified(true);
+            $societyUser->setRoles(["ROLE_SOCIETY"]);
+
+
+            $user = new User();
+            $user->setName($faker->firstName());
+            $user->setLastName($faker->lastName());
+            $user->setEmail('user' . $i . '@gmail.com');
+            $user->setPassword($this->passwordHasher->hashPassword($user, SELF::PASSWORD));
+            $user->setCreatedAt(new DateTime());
+            $user->setSociety($society);
+            $user->setIsVerified(true);
+            $manager->persist($user);
+            $manager->persist($admin);
+            $manager->persist($accountant);
+            $manager->persist($societyUser);
+            $manager->flush();
+        }
     }
 }
