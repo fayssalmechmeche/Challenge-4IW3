378036e63196d1475cdbc69695ac7e2934d54e00
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2024-02-10 14:06:54 +0100

    fix pre merge

--- src/Form/Admin/SocietyType.php
+++ src/Form/Admin/SocietyType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
